796c2ede2f523065e4ec2b581fa2b9d9fea83a66
Dan Webb <dan.webb@damacus.io>
2016-11-04 14:09:47 +0000

    Regex match the user-Agent the user-agent changes depending on platform/ruby version (which is actually kind of nice)

--- spec/opsview_rest_spec.rb
+++ spec/opsview_rest_spec.rb
@@ -36,7 +36,7 @@ describe OpsviewRest do
   describe '#list' do
     it 'returns list of hosts by default' do
       stub_request(:get, "https://example.com/rest/config/host?rows=all").
-         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Content-Type'=>'application/json', 'Host'=>'example.com', 'User-Agent'=>'rest-client/2.0.0', 'X-Opsview-Token'=>'88dffa0974c364e56431697f257564fb1524b029', 'X-Opsview-Username'=>'hi'}).
+         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Content-Type'=>'application/json', 'Host'=>'example.com', 'User-Agent'=> /rest-client\/2\.0\.0.*/, 'X-Opsview-Token'=>'88dffa0974c364e56431697f257564fb1524b029', 'X-Opsview-Username'=>'hi'}).
          to_return(:status => 200, :body => fixture('list'), :headers => {})
       list_response = opsview_rest.list
       list_response.to_s.should include "Network - Base", "Monitoring Servers", "/images/logos/opsview_small.png", "Application - Opsview Master"
@@ -44,7 +44,7 @@ describe OpsviewRest do
 
     it 'returns a full list for a given value' do
       stub_request(:get, "https://example.com/rest/config/hosttemplate?rows=all").
-        with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Content-Type'=>'application/json', 'Host'=>'example.com', 'User-Agent'=>'rest-client/2.0.0', 'X-Opsview-Token'=>'88dffa0974c364e56431697f257564fb1524b029', 'X-Opsview-Username'=>'hi'}).
+        with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Content-Type'=>'application/json', 'Host'=>'example.com', 'User-Agent'=> /rest-client\/2\.0\.0.*/, 'X-Opsview-Token'=>'88dffa0974c364e56431697f257564fb1524b029', 'X-Opsview-Username'=>'hi'}).
         to_return(:status => 200, :body => fixture('list_hosttemplate'))
       list_response_hosttemplate = opsview_rest.list(:type => 'hosttemplate')
       list_response_hosttemplate.to_s.should include "Opsview Housekeeping Cronjob Monitor", "Microsoft Active Directory", "Apache current requests"
@@ -55,7 +55,7 @@ describe OpsviewRest do
     it 'returns current reload status' do
       pending 'Gives no response'
       stub_request(:get, "https://example.com/rest/reload").
-         with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'Content-Type'=>'application/json', 'User-Agent'=>'Ruby', 'X-Opsview-Token'=>'88dffa0974c364e56431697f257564fb1524b029', 'X-Opsview-Username'=>'hi'}).
+         with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'Content-Type'=>'application/json', 'User-Agent'=> /rest-client\/2\.0\.0.*/, 'X-Opsview-Token'=>'88dffa0974c364e56431697f257564fb1524b029', 'X-Opsview-Username'=>'hi'}).
          to_return(:status => 200, :body => fixture('reload'))
 
       opsview_rest.reload
