f433b2cae6632da1ef0a2c0728788e3d2f8fa0e7
Dan Webb <dan.webb@damacus.io>
2017-06-22 21:41:06 +0300

    Lint using Rubocop (#18)

--- lib/opsview_rest/notificationmethod.rb
+++ lib/opsview_rest/notificationmethod.rb
@@ -2,30 +2,28 @@ require 'opsview_rest/mixin'
 
 class OpsviewRest
   class NotificationMethod
-
     include OpsviewRest::Mixin
 
     attr_accessor :options, :opsview, :resource_type
 
     def initialize(opsview, options = {})
       @options = {
-        :name => "Unknown",
-        :master => false,
-        :active => true,
-        :command => "notify_by_email",
-        :contact_variables => "EMAIL",
-        :save    => true,
-        :replace => false
+        name: 'Unknown',
+        master: false,
+        active: true,
+        command: 'notify_by_email',
+        contact_variables: 'EMAIL',
+        save: true,
+        replace: false
       }.update options
 
       @opsview = opsview
       @resource_type = @options[:type]
 
-      @options[:master] = if @options[:master] then 1 else 0 end
-      @options[:active] = if @options[:active] then 1 else 0 end
+      @options[:master] = (@options[:master] ? 1 : 0)
+      @options[:active] = (@options[:active] ? 1 : 0)
 
       save(@options[:replace]) if @options[:save]
     end
-
   end
 end
